bc7fe330555ec84a49f10b8b9c9963dac02a8366
Keith Robertson <no@no.com>
2013-04-03 20:31:59 -0600

    GPS initial

---
+++ GPS.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <time.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "GPS.h"
+
+#define NS_PER_MS	1000000
+#define NS_PER_S	1000000000
+#define DATA_COL_PERIOD_MS	250
+
+
+//Compass class
+
+GPS::GPS() {
+	subsys_name = GPS_NAME;
+	subsys_num = SUBSYS_GPS;
+}
+
+GPS::~GPS(){
+	close(gps_fd);
+}
+
+void GPS::init_sensor() {
+	sprintf(gps_filepath,"/dev/ttyO2");
+	if ((gps_fd = open(gps_filepath,O_RDWR)) < 0) {
+		perror("Failed to open the bus for GPS read.\n");
+	}
+}
+
+float GPS::data_grab(){
+	char buff[2];
+	if(read(gps_fd,buff,2) != 2) {
+		perror("Failed to read data from compass");
+	}
+	return ((float)(((int)buff[0] << 8) + (int)buff[1]));
+}
+
+void GPS::collector(){
+	float gps_reading;
+	struct timespec t;
+	clock_gettime(CLOCK_MONOTONIC ,&t);
+	while(1){
+		t.tv_nsec+= DATA_COL_PERIOD_MS*NS_PER_MS;
+		while(t.tv_nsec > NS_PER_S){
+			t.tv_sec++;
+			t.tv_nsec -= NS_PER_S;
+		}
+		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, NULL);
+		gps_reading = data_grab();
+		std::cout << "GPS Heading: " << gps_reading << std::endl;
+	}
+}
+
+void GPS::analysis(){
+	struct timespec t;
+	send_sys_message((char *)std::string("hello from GPS analysis").c_str());
+	clock_gettime(CLOCK_MONOTONIC ,&t);
+	t.tv_sec++;
+	clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, NULL);
+	send_sys_message((char *)std::string("hello from GPS analysis 1 second later").c_str());
+}
+
+void GPS::handle_message(char* message){
+	std::cout << "GPS Recieved Message:" << std::endl;
+	std::cout << message <<std::endl << std::endl;
+}
---
+++ GPS.h
@@ -0,0 +1,62 @@
+#ifndef	GPS_H_
+#define	GPS_H_
+
+#include "Sensor.h"
+
+#define GPS_NAME	"GPS"
+#define SUBSYS_GPS	1
+
+/**
+ * \class GPS
+ * \brief GPS data collection and analysis
+ * 
+ * Collects data from the GPS and analyzes the data. Sends system 
+ * messages to control actuators
+ */
+class GPS : public Sensor {
+	public:
+	
+		/**
+		 * \brief GPS default constructor
+		 * 
+		 * sets subsystem parameters
+		 */
+		GPS();
+		
+		~GPS();
+		
+		/**
+		 * \brief grabs data from GPS
+		 * 
+		 * Gets data from GPS over UART
+		 */
+		float data_grab();
+		
+		void init_sensor();
+		
+		/**
+		 * \brief collects GPS data
+		 * 
+		 * collects GPS data and averages data prior to analysis
+		 */
+		void collector();
+		
+		/**
+		 * \brief data analysis
+		 * 
+		 * Issues commands to acuators based on GPS data
+		 */
+		void analysis();
+		
+	protected:
+	
+		/**
+		 * \brief handles messages sent to the GPS subsystem
+		 */
+		void handle_message(char* message);
+		
+		int gps_fd;
+		char gps_filepath[40];
+};
+
+#endif
